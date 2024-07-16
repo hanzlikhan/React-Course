27c96399dd0afed0beaa65fbfd6b361637748a3c
hanzlikhan <khangormani79@gmail.com>
2024-07-16 16:08:45 +0500

    init

---
+++ React-2/react-bootstrap/src/components/AddItem2.jsx
@@ -0,0 +1,18 @@
+function AddItem2() {
+  var itemName = "Go to College";
+  var date = "16/07/2024";
+  return (
+    <div className="container text-container">
+      <div className="row kg-row">
+        <div className="col-6">{itemName}</div>
+        <div className="col-4">{date}</div>
+        <div className="col-2">
+          <button type="button" className="btn btn-danger kg-button">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AddItem2;
