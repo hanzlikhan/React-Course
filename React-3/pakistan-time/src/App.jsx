25396af4118d124ab93802050f9681d2f3b77af9
hanzlikhan <khangormani79@gmail.com>
2024-07-16 16:10:11 +0500

    init

---
+++ React-3/pakistan-time/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import MianName from './components/MianName';
+import CentralPara from './components/CentralPara';
+import ShowTime from './components/ShowTime';
+function App() {
+
+  return (
+    <div>
+      <MianName></MianName>
+      <CentralPara></CentralPara>
+      <ShowTime></ShowTime>
+    </div>
+  )
+}
+
+export default App
