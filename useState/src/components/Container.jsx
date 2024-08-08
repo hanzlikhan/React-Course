5f23a6a84a70997ec20a1179add73af94140ee54
hanzlikhan <khangormani79@gmail.com>
2024-08-08 22:33:18 +0500

    init

---
+++ useState/src/components/Container.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Container.module.css';
+
+function Container(props) {
+  return <div className={styles.Container}>{props.children}</div>;
+}
+
+export default Container;
